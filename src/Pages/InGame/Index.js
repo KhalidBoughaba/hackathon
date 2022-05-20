ec7f22c907e88a9910d4426758e4a554074970b0
harbouli <mharbouli@icloud.com>
2022-05-20 15:41:11 +0100

    update

--- src/Pages/InGame/Index.js
+++ src/Pages/InGame/Index.js
@@ -89,7 +89,7 @@ export default function Index(i) {
               addChoice(answer);
             }}
           >
-            {answer}
+            {/* {answer} */}
           </div>
         ))}
       </div>
